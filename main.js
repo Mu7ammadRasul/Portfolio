88aa84a7eb03e333a2952764872e790935326b5e
Mu7ammadRasul <muhammad.rasul23@yahoo.com>
2021-11-02 12:26:59 +0300

    home section is done with some animations ready, Alhamdulilah

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const right = ScrollReveal({origin: 'left', distance: '100px',
+duration:2000, reset: true
+})
+
+right.reveal('.Centeredtext',{})
+right.reveal('.Centeredtext h2',{delay:200})
+right.reveal('.Centeredtext h4',{delay:100})
+right.reveal('.explore a',{delay:90})
+right.reveal('.socials li a',{interval:200})
+right.reveal('.drawer li',{interval:70})
