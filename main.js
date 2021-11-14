5984e2f514608c174df527db24a4420a936789dd
Mu7ammadRasul <muhammad.rasul23@yahoo.com>
2021-11-14 16:44:42 +0300

    some essential changes, adding islamic and universe section, have to arrange work section as well

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 
-const right = ScrollReveal({origin: 'left', distance: '100px',
+const l = ScrollReveal({origin: 'left', distance: '100px',
 duration:2000, reset: true
 })
 
-right.reveal('.Centeredtext',{})
-right.reveal('.Centeredtext h2',{delay:200})
-right.reveal('.Centeredtext h4',{delay:100})
-right.reveal('.explore a',{delay:90})
-right.reveal('.socials li a',{interval:200})
-right.reveal('.work-reveal img', {interval:200})
-right.reveal('.work-text h2', {delay:200})
-right.reveal('.work-text .p1', {delay:300})
-right.reveal('.work-text .p2', {delay:300})
+l.reveal('.Centeredtext',{})
+l.reveal('.Centeredtext h2',{delay:200})
+l.reveal('.Centeredtext h4',{delay:100})
+l.reveal('.explore a',{delay:90})
+l.reveal('.socials li a',{interval:200})
+l.reveal('.work-reveal img', {interval:200})
+l.reveal('.work-text h2', {delay:200})
+l.reveal('.work-text .p1', {delay:300})
+l.reveal('.work-text .p2', {delay:300})
+
+const t = ScrollReveal({origin: 'top', distance: '50px',
+duration:2000, reset: true
+})
+
+t.reveal('.other-works .box', {interval:150})
